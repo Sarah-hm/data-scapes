a3ddc28285a2d034ef5e9bc9d5576aa150c4fa90
Sarah Hontoy-Major <sarah.hm@hotmail.ca>
2023-03-22 14:17:21 -0400

    doing some things

--- website/js/ds-script.js
+++ website/js/ds-script.js
@@ -2,17 +2,30 @@ window.onload = (event) => {
 
 let datascapesContainer = document.querySelector("#data-scapes-container");
 let datascapesContainerOverlays = document.querySelectorAll(".title-overlay-zoom-out")
+let blackoutScreen = document.querySelector("#black-out-screen")
+let scrollinstruc = document.querySelector("#scroll-instruction")
 
+//Make scroll instruction appear after a few seconds
+setTimeout(()=>{
+    scrollinstruc.style.color="rgba(0,0,0,1)"
+}, 1000)
+blackoutScreen.style.backgroundColor = "rgba(0,0,0,0)";
+setTimeout(()=>{
+    blackoutScreen.style.display = "none"
+})
 
 document.addEventListener("scroll", () =>{
 console.log("scrolled")
 datascapesContainer.style.transform = "scale(0.25)"
-
+scrollinstruc.style.display = "none"
 datascapesContainerOverlays.forEach((el) => {
 console.log(el);
 el.style.opacity = "1"
 })
-//body.style.background = "black"
+
+blackoutScreen.style.display = "inline"
+blackoutScreen.style.blackgroundColor = "rgba(0,0,0,1)"
+// body.style.background = "black"
 setTimeout(sendToRhizomeCloud, 2000)
 })
 
--- website/js/rhizome-script.js
+++ website/js/rhizome-script.js
@@ -1,33 +1,43 @@
 window.onload = (event) => { 
 
-let datascapesContainer = document.querySelector("#data-scapes-container");
-let datascapesContainerOverlays = document.querySelectorAll(".rhizome-grid-item")
+let rhizomeItems = document.querySelectorAll(".rhizome-grid-item");
+let blackoutScreen = document.querySelector("#black-out-screen")
 
 
+blackoutScreen.style.backgroundColor = "rgba(0,0,0,1)"
+setTimeout(()=>{
+blackoutScreen.style.display = "none"
+}, 1500)
 
 
-datascapesContainerOverlays.forEach((el) => {
+//For earch rhizome items, interact with them on hover and link them to their specific url
+rhizomeItems.forEach((el) => {
     console.log(el)
+    // On mouse enter, make title disappear and pullquote appear
     el.addEventListener("mouseenter", ()=>{
        el.querySelector("h1").style.color = "rgba(0,0,0,0)"
        setTimeout(()=>{
         el.querySelector("h2").style.color = "rgba(0,0,0,1)"
        }, 750)
     })
+    //On mouse leave, make pullquote disappear and title appear
     el.addEventListener("mouseleave", ()=>{
         el.querySelector("h2").style.color = "rgba(0,0,0,0)"
         setTimeout(()=>{
          el.querySelector("h1").style.color = "rgba(0,0,0,1)"
         }, 750)
      })
-     console.log(el.getAttribute("data-url"))
+//  when clicked, send user to whatever url attributed to the grid-item clicked
      el.addEventListener("click", ()=>{
-        window.location.href = `${el.getAttribute("data-url")}`
+
+        el.querySelector("h1").style.color = "rgba(0,0,0,0)"
+        el.querySelector("h2").style.color = "rgba(0,0,0,0)"
+        el.classList.toggle("grid-item-open");
+        setTimeout(()=>{
+         window.location.href = `${el.getAttribute("data-url")}`
+        }, 1000)
+       
      })
     })
 
 }//window onload
-
-function sendTodatascapesContainer(){
- window.location.href = 'index.html';
-}
